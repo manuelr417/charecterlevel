61ae3d9e48c83e86d71ef36fc2a7bfffdf760871
manuelrodriguez <manuel.rodriguez@upr.edu>
2021-03-23 14:04:54 -0400

    Bunch of CNN

--- charCNN2.py
+++ charCNN2.py
@@ -81,7 +81,7 @@ X = Dense(32, activation='relu', name="dense2")(X)
 output = Dense(1, activation='linear', name="dense3")(X)
 
 model = Model(inputs=input_layer, outputs=output)
-model.compile(optimizer='adam', loss='mae', metrics=['mse', 'mae']) # Adam, categorical_crossentropy
+model.compile(optimizer='adam', loss='mse', metrics=['mse', 'mae']) # Adam, categorical_crossentropy
 model.summary()
 
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
