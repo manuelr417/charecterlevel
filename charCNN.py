49f3a7ae35dcce2f771e494ba3c48e19ce1ebe01
manuelrodriguez <manuel.rodriguez@upr.edu>
2021-03-26 09:56:29 -0400

    Bunch of CNN

--- charCNN.py
+++ charCNN.py
@@ -104,7 +104,7 @@ model.summary()
 
 #model.fit(np_data, y_data, epochs=50, batch_size= 64, validation_split=0.3, callbacks=[tensorboard_callback])
 
-history = model.fit(np_data, y_data, epochs=5, batch_size= 64, validation_split=0.3)
+history = model.fit(np_data, y_data, epochs=10, batch_size= 64, validation_split=0.3)
 
 hist = pd.DataFrame(history.history)
 hist['epoch'] = history.epoch
